c2caa33934861d3e2020e21df617629a3cac1bc3
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2022-01-07 23:59:35 +0530

    Final Commit

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -26,7 +26,7 @@ let getContent = (note)=>{
 
 const ListItem = ({note}) => {
     return (
-        <Link to={`${api_url}/api/note/${note.id}`}>
+        <Link to={`/note/${note.id}`}>
             <div className="notes-list-item">
             <h3>{getTitle(note)}</h3>
             <p><span>{getTime(note.updated)}</span>{getContent(note)}</p>
